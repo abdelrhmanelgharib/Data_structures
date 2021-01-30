9cb80be582d9808e0b8945b3dd9387afdf9694a7
abdelrhmanelgharib <50600176+abdelrhmanelgharib@users.noreply.github.com>
2021-01-30 03:23:29 +0200

    still work on it

---
+++ StudentSytem/DataTypes.h
@@ -0,0 +1,25 @@
+/*
+ * DataType.h
+ *
+ *  Created on: NOV 30, 2020
+ *      Author: Elgharib
+ */
+
+#ifndef _DATATYPES_H_ // if not define
+#define _DATATYPES_H_
+
+
+typedef unsigned char u8;
+typedef signed char s8;
+typedef unsigned short int u16;
+typedef signed short int s16;
+typedef unsigned long int u32;
+typedef signed long int s32;
+typedef unsigned long long int u64;
+typedef signed long long int s64;
+typedef   float f32;
+typedef   double f64;
+
+
+
+#endif
